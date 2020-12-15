d6836ef5986393a4ce6905b90d9eef34d4d7cf9a
Abraham <abrahamciclo@gmail.com>
2020-12-15 11:48:04 +0100

    Servicios

---
+++ src/main/java/com/lvldungeons/service/PersonajeService.java
@@ -0,0 +1,54 @@
+package com.lvldungeons.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lvldungeons.model.entity.Personaje;
+import com.lvldungeons.model.entity.User;
+import com.lvldungeons.model.repo.PersonajeRepository;
+
+@Service
+public class PersonajeService {
+
+	//Repositorios
+		@Autowired
+		private PersonajeRepository persoRepo;
+		
+		//Servicios
+		@Autowired
+		private UpdateService updateService;
+		
+		//Get de todos los personajes
+			public List<Personaje> getEntity() {
+				 return (List<Personaje>)persoRepo.findAll();
+			}
+			
+			
+		//Get de un user por id
+			public Personaje getEntityById(Long id) {
+				return persoRepo.findById(id).get();
+			}
+			
+			//Crear un nuevo user
+			public Personaje saveEntity(Personaje sent) {
+				return persoRepo.save(sent);
+			}
+			
+			//Actualziar un user
+			public Personaje updateEntity(Long id, Personaje sent) {
+				Personaje us = persoRepo.findById(id).get();
+				
+				if(us !=null) {
+					updateService.updatePersonaje(us, sent);
+					persoRepo.save(us);
+				}
+				return us;
+			}
+			
+			//Borrar un user
+			public void deleteEntity(Long id) {
+				persoRepo.deleteById(id);
+			}
+}
---
+++ src/main/java/com/lvldungeons/service/UserService.java
@@ -0,0 +1,57 @@
+package com.lvldungeons.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+import com.lvldungeons.model.entity.User;
+import com.lvldungeons.model.repo.UserRepository;
+
+
+@Service
+public class UserService {
+
+	
+	//Repositorios
+	@Autowired
+	private UserRepository userRepo;
+	
+	//Servicios
+	@Autowired
+	private UpdateService updateService;
+	
+	//Get de todos los users
+		public List<User> getEntity() {
+			 return (List<User>)userRepo.findAll();
+		}
+		
+		
+	//Get de un user por id
+		public User getEntityById(Long id) {
+			return userRepo.findById(id).get();
+		}
+		
+		//Crear un nuevo user
+		public User saveEntity(User sent) {
+			return userRepo.save(sent);
+		}
+		
+		//Actualziar un user
+		public User updateEntity(Long id, User sent) {
+			User us = userRepo.findById(id).get();
+			
+			if(us !=null) {
+				updateService.updateUser(us, sent);
+				userRepo.save(us);
+			}
+			return us;
+		}
+		
+		//Borrar un user
+		public void deleteEntity(Long id) {
+			userRepo.deleteById(id);
+		}
+}

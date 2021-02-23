fedb3f676810c06042313c98b35c17555531c8cb
Ruben <rudahee@gmail.com>
2021-02-23 12:36:07 +0100

    prepare to deploy in external tomcat

--- src/main/java/com/lvldungeons/LvLDungeonsApplication.java
+++ src/main/java/com/lvldungeons/LvLDungeonsApplication.java
@@ -2,12 +2,13 @@ package com.lvldungeons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class LvLDungeonsApplication {
+public class LvLDungeonsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LvLDungeonsApplication.class, args);
